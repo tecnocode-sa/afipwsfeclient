a2e99bf6e2b8545c886ed4b707b1cb2712da98cf
Matias Dieguez <matiasdieguez@hotmail.com>
2019-10-27 18:02:26 -0300

    Agregado de WSCDC

---
+++ WscdcClient.cs
@@ -0,0 +1,28 @@
+﻿using System.ServiceModel;
+using System.Threading.Tasks;
+
+namespace AfipWsfeClient
+{
+    public class WscdcClient
+    {
+        public bool IsProdEnvironment { get; set; } = false;
+        public long Cuit { get; set; }
+        public string Token { get; set; }
+        public string Sign { get; set; }
+
+        public string WscdcUrlHomologation { get; set; } = "https://wswhomo.afip.gov.ar/WSCDC/service.asmx";
+        public string WscdcUrlProd { get; set; } = "https://servicios1.afip.gov.ar/WSCDC/service.asmx";
+
+        public async Task<AfipWscdcServiceReference.ComprobanteConstatarResponse> ComprobanteConstatarAsync(AfipWscdcServiceReference.CmpDatos cmpReq)
+        {
+            var wscdcService = new AfipWscdcServiceReference.ServiceSoapClient(AfipWscdcServiceReference.ServiceSoapClient.EndpointConfiguration.ServiceSoap);
+            wscdcService.Endpoint.Address = new EndpointAddress(IsProdEnvironment ? WscdcUrlProd : WscdcUrlHomologation);
+
+            var auth = new AfipWscdcServiceReference.CmpAuthRequest { Cuit = Cuit, Sign = Sign, Token = Token };
+
+            var response = await wscdcService.ComprobanteConstatarAsync(auth, cmpReq);
+
+            return response;
+        }
+    }
+}
